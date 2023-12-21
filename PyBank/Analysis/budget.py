5d70fb4e239d3fe2a6d489fd5fdb6fb521db1ab0
Shakermaker001 <142272543+Shakermaker001@users.noreply.github.com>
2023-12-21 15:35:01 -0600

    done?

--- PyBank/Analysis/budget.py
+++ PyBank/Analysis/budget.py
@@ -3,6 +3,7 @@ import csv
 
 
 csvpath = os.path.join('..', 'Resources','budget_data.csv')
+output_path = os.path.join('..','Analysis','bank.txt')
 
 with open(csvpath) as csvfile:
 
@@ -37,6 +38,25 @@ with open(csvpath) as csvfile:
     print(f"Avereage Revenue Change: $", round(avg_rev_change))
     print(f"Greatest Increase in Revenue:", max_rev_change_date,"($", max_rev_change,")")
     print(f"Greatest Decrease in Revenue:", min_rev_change_date,"($", min_rev_change,")")
+
+
+
+with open (output_path, 'w') as f:
+    f.write('Financial Analysis')
+    f.write('\n')
+    f.write('------------------')
+    f.write('\n')
+    f.write("Total Months: 86")
+    f.write('\n')
+    f.write("Total Revenue: $ 22564198.0")
+    f.write('\n')
+    f.write('Average Revenue Change: $-8311')
+    f.write('\n')
+    f.write("Greatest Increase in Revenue: Jul-16 ($ 1862002.0 )")
+    f.write('\n')
+    f.write("Greatest Decrease in Revenue: Jan-14 ($ -1825558.0 )")
+
+    
 
 
         
--- PyPoll/Analysis/poll.py
+++ PyPoll/Analysis/poll.py
@@ -3,17 +3,18 @@ import csv
 from collections import Counter
 
 csvpath = os.path.join('..', 'Resources','election_data.csv')
+output_path = os.path.join('..','Analysis','poll.txt')
 
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter = ',')
 
     next(csvreader)
-    
+# Gathering votes and canidate names
     canidates = [row[2] for row in csvreader]
     canidate_votes = Counter(canidates)
     total_votes = len(canidates)
-
+# Using list comprehension to build result string
     election_results = [f'{value}: {(count / total_votes) * 100:.2f}% ({count})'\
                          for value, count in canidate_votes.items()]
     
@@ -26,6 +27,25 @@ for result in election_results:
 print('----------------')
 print('Winner: Diana DeGette')
 print('----------------')
+
+with open (output_path, 'w') as f:
+    f.write('Election Results')
+    f.write('\n')
+    f.write('------------------')
+    f.write('\n')
+    f.write("Total Votes: 369711")
+    f.write('\n')
+    f.write("------------------")
+    f.write('\n')
+    f.write('Charles Casper Stockham: 23.05% (85213)')
+    f.write('\n')
+    f.write("Diana DeGette: 73.81% (272892)")
+    f.write('\n')
+    f.write("Raymon Anthony Doane: 3.14% (11606)")
+    f.write('\n')
+    f.write('------------------')
+    f.write('\n')
+    f.write('Winner: Diana DeGette')
 
 
 
